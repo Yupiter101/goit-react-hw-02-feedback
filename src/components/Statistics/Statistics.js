9b422d939128c46a14a79a829a738ad763cd4c27
Sergey_Sanyuk <shv08@ukr.net>
2023-06-07 17:00:46 +0300

    V4 css.module
     css.module

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,18 +1,19 @@
 
 import PropTypes from 'prop-types';
-import "./Statistics.css";
+import css from "./Statistics.module.css";
+
 
 
 function Statistics({good, neutral, bad, total, positivePercentage}) {
     return (
       <>
-        <ul className="StatList">
-          <li className="StatItem">good:<span>{good}</span></li>
-          <li className="StatItem">neutral:<span>{neutral}</span></li>
-          <li className="StatItem">bad:<span>{bad}</span></li>
+        <ul className={css.StatList}>
+          <li className={css.StatItem}>good:<span>{good}</span></li>
+          <li className={css.StatItem}>neutral:<span>{neutral}</span></li>
+          <li className={css.StatItem}>bad:<span>{bad}</span></li>
 
-          <li className="StatItem">Total:<span>{total()}</span></li>
-          <li className="StatItem">Positve feedback:<span>{positivePercentage()}%</span></li>
+          <li className={css.StatItem}>Total:<span>{total()}</span></li>
+          <li className={css.StatItem}>Positve feedback:<span>{positivePercentage()}%</span></li>
         </ul>
       </>
     )
